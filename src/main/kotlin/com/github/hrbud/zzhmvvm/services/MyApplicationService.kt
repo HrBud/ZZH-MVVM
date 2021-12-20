f16c8958da471cc5840497224f4cd39560b3d4b0
GitHub Action <action@github.com>
2021-12-20 16:56:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/hrbud/zzhmvvm/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hrbud/zzhmvvm/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hrbud.zzhmvvm.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hrbud.zzhmvvm.MyBundle
 
 class MyApplicationService {
 
